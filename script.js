b43d6faf0cbd306e8303b14efc6ac513b623e280
tpires1041 <131817259+tpires1041@users.noreply.github.com>
2023-05-03 17:21:42 -0400

    Aula de 03 05 23

--- script.js
+++ script.js
@@ -8,17 +8,27 @@ class Produto {
     }
 
     mostrarProduto(){
-
+        return this.nome + this.data_de_cadastro + this.descricao + this.preco
     }
+    
+    
 }
 
 class ProdutoDestaque extends Produto {
-    constructor(nome, data_de_cadastro, descricao, preco, imagem_destaque){
+    constructor(nome, data_de_cadastro, descricao, preco){
         super(nome, data_de_cadastro, descricao, preco)
-        this.imagem_destaque = imagem_destaque
-    }
-    
-    mostrarProdutoDestaque(){
-        
-    }
 }
+
+
+mostrarProdutoDestaque(){
+    return `
+            <h1 class="teste">${this.nome}</h1>`
+            //return this.nome + this.data_de_cadastro + this.descricao + this.preco
+}
+
+}
+
+const produto = new ProdutoDestaque("Opala", "05/03/23", "Carro massa", 1000000)
+console.log(produto.mostrarProdutoDestaque());
+const produtoDestaque = document.getElementById("produtoDestaque");
+produtoDestaque.insertAdjacentHTML('afterbegin', produto.mostrarProdutoDestaque());
