6350a1371a3568b6f1f24c75acd365be153d0dc4
tpires1041 <tiagopires2501@outlook.com>
2023-05-23 22:19:27 -0400

    Corrigi muitaaa coisa 23 05 23

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ class Produto {
     mostrarProduto(){
         return `
                 <h1 class="nome">${this.nome}</h1>
-                <h2 class="descricao">${this.descricao}</h2>`
+                <h2 class="preco">R$${this.preco}</h1>
+                <h3 class="descricao">${this.descricao}</h3>
+                <h4 class="data_de_cadastro">${this.data_de_cadastro}</h4>`
         //return this.nome + this.data_de_cadastro + this.descricao + this.preco
 }
 }
@@ -23,21 +25,29 @@ class ProdutoDestaque extends Produto {
 
 mostrarProdutoDestaque(){
     return `
-            <h1 class="nome">${this.nome}</h1>
-            <img src="opaleira.jpg" height="300px" width=auto>
-            <h2 class="descricao">${this.descricao}</h2>`
+            <h2 class="nome">${this.nome}</h2>
+            <img src="opala.jpg" height="300px" width=auto>
+            <h2 class="preco">R$${this.preco}</h1>
+            <h3 class="descricao">${this.descricao}</h3>
+            <h4 class="data_de_cadastro">${this.data_de_cadastro}</h4>`
             //return this.nome + this.data_de_cadastro + this.descricao + this.preco
 }
 
 }
 
-const opala = new ProdutoDestaque("Opala", "05/03/23", "Carro massa", 100000)
-console.log(opala.mostrarProdutoDestaque());
+const opala = new ProdutoDestaque("Chevrolet Opala", "05/03/23", "Carro massa.", 100000)
 const produtoDestaque = document.getElementById("produtoDestaque");
 produtoDestaque.insertAdjacentHTML('afterbegin', opala.mostrarProdutoDestaque());
 
 
-const fusca = new Produto("Fusca", "25/01/23", "Fusca muito bem cuidado", 40000)
-console.log(fusca.mostrarProduto());
+const fusca = new Produto("Volkswagen Fusca", "25/01/23", "Fusca muito bem cuidado.", 40000);
 const produto1 = document.getElementById("listaProdutos");
-produtoDestaque.insertAdjacentHTML('afterbegin', fusca.mostrarProduto());
+produto1.insertAdjacentHTML('beforeend', fusca.mostrarProduto());
+
+const maverick = new Produto("Ford Maverick", "20/11/22", "Maverick 1974 preparado.", 200000);
+const produto2 = document.getElementById("listaProdutos");
+produto2.insertAdjacentHTML('beforeend', maverick.mostrarProduto());
+
+const kombi = new Produto("Volkswagen Kombi", "20/05/22", "Aceito troca.", 42000);
+const produto3 = document.getElementById("listaProdutos");
+produto3.insertAdjacentHTML('beforeend', kombi.mostrarProduto());
